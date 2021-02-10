e404762028b1aa81e6362fce7ae0696b83747707
oukumben <ummut22005@gmail.com>
2021-02-10 19:47:49 +0300

    Core

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Core.DataAccess;
+using Entities.Concrete;
+using Entities.Concrete.DTOs;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal : IEntityRepo<Car>
+    {
+        List<CarDTO> GetCarDetails();
+    }
+}
